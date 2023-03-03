9435deff65e05b21ea8d201fc8d3f1dfa952cc4a
SHakirPY <101662958+shakirPYJV@users.noreply.github.com>
2023-03-03 16:30:57 +0600

    Add files via upload
    Solution to CQ no 4 question no IV

---
+++ college 4 IV.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+    {
+        float h,b,area;
+        printf("Enter the values of h,b:");
+        scanf("%f.%f",&h,&b);
+
+        area = 0.5*h*b;
+        printf("The area is %f",area);
+        getch();
+        return 0;S
+    }
